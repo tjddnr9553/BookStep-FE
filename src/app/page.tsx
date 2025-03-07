40f154b5c1be138722070c4b60b8b45f44c2a6a4
tjddnr9553 <tjddnr9553@naver.com>
2025-03-07 15:39:40 +0900

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <p>서버 : {process.env.NEXT_PUBLIC_API_SERVER_URL}</p>
+      <p>서버 : {process.env.NEXT_PUBLIC_API_SERVER_URL}</p>
+    </div>
+  )
+}
