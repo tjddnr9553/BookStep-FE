df787d7ddffa40eb657b418f8f984453bac6ff1e
tjddnr9553 <tjddnr9553@naver.com>
2025-03-08 12:07:23 +0900

    [Test] CI/CD 개인 레포지토리 연동 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p>서버 : {process.env.NEXT_PUBLIC_API_SERVER_URL}</p>
-      <p>서버 : {process.env.NEXT_PUBLIC_API_SERVER_URL}</p>
+      CI/CD 테스트
     </div>
   )
 }
