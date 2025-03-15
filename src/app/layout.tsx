71bd59d71228c7de65c9e15b9e3432b0b826b015
tjddnr9553 <tjddnr9553@naver.com>
2025-03-15 21:38:50 +0900

    [Feat] 메인 레이아웃(Header, Sidebar) 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
-import "./globals.css";
+import './globals.css'
+import Sidebar from '@/components/sidebar'
+import Header from '@/components/header'
+import styles from '@/app/layout.module.css'
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <html lang="en">
-        <body>
-        {children}
-        </body>
-        </html>
-    );
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <html lang="en">
+    <body>
+    <div className={styles.layoutContainer}>
+      <div className={styles.sidebarWrapper}>
+        <Sidebar />
+      </div>
+      <div className={styles.contentWrapper}>
+        <Header />
+        <main className={styles.content}>
+          {children}
+        </main>
+      </div>
+    </div>
+    </body>
+    </html>
+  )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '@/components/header.module.css'
+
+export default function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerLogo}>Logo</div>
+      <div className={styles.iconButton}>
+        <Link href={'/'}>
+          <Image src="./svgs/search.svg" alt={'Search'} width={22.5} height={22.5} />
+        </Link>
+      </div>
+      <div className={styles.iconButton}>
+        <Link href={'/'}>
+          <Image src="./svgs/notification.svg" alt={'Notification'} width={22.5} height={22.5} />
+        </Link>
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.iconButton}>
+        <Link href={'/'}>
+          <Image src="./svgs/avatar.svg" alt={'Avatar'} width={22.5} height={22.5} />
+        </Link>
+      </div>
+      <div className={styles.userName}>
+        Sarah
+      </div>
+      <div className={styles.userDropdownButton}>
+        <Image src="./svgs/arrow_down.svg" alt={'Dropdown'} width={22.5} height={22.5} />
+      </div>
+    </div>
+  )
+}
