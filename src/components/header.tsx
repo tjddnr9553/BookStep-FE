553e46937e723367fcc3eb4d0da72281f614615b
tjddnr9553 <tjddnr9553@naver.com>
2025-03-25 18:59:16 +0900

    [Design] 헤더 레이아웃 리디자인

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,34 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '@/components/header.module.css'
+import styles from '@/components/header.module.scss'
 
 export default function Header() {
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.headerLogo}>Logo</div>
-      <div className={styles.iconButton}>
-        <Link href={'/'}>
-          <Image src="/svgs/search.svg" alt={'Search'} width={22.5} height={22.5} />
-        </Link>
-      </div>
-      <div className={styles.iconButton}>
-        <Link href={'/'}>
-          <Image src="/svgs/notification.svg" alt={'Notification'} width={18} height={22} />
-        </Link>
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.iconButton}>
-        <Link href={'/'}>
-          <Image src="/svgs/avatar.svg" alt={'Avatar'} width={22.5} height={22.5} />
-        </Link>
-      </div>
-      <div className={styles.userName}>
-        Sarah
-      </div>
-      <div className={styles.userDropdownButton}>
-        <Image src="/svgs/dropdown.svg" alt={'Dropdown'} width={22.5} height={22.5} />
+    <div className={styles.header__container}>
+      <div className={styles.header__logo}>Logo</div>
+      <div className={styles.header__icons}>
+        <div className={styles.icon}>
+          <Link href={'/'}>
+            <Image src="/svgs/search.svg" alt={'Search'} width={22.5} height={22.5} />
+          </Link>
+        </div>
+        <div className={styles.icon}>
+          <Link href={'/notification'}>
+            <Image src="/svgs/notification.svg" alt={'Notification'} width={18} height={22} />
+          </Link>
+        </div>
+        <hr className={styles.divider} />
+        <div className={styles.icon}>
+          <Link href={'/mypage'}>
+            <Image src="/svgs/avatar.svg" alt={'Avatar'} width={22.5} height={22.5} />
+          </Link>
+        </div>
+        <div className={styles.userName}>
+          UserName
+        </div>
+        <div className={styles.userDropdownButton}>
+          <Image src="/svgs/dropdown.svg" alt={'Dropdown'} width={22.5} height={22.5} />
+        </div>
       </div>
     </div>
   )
